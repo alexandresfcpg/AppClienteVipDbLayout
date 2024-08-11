c3e1a31209cf5b95f11e7eae39ca3730e7bd1a71
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-11 19:40:43 -0300

    Criando uma tela de background gradiente para inserção desse efeito nas telas.

---
+++ app/src/main/java/app/daazi/aluno/appclientevipdblayout/view/ModeloActivity.java
@@ -0,0 +1,16 @@
+package app.daazi.aluno.appclientevipdblayout.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.daazi.aluno.appclientevipdblayout.R;
+
+public class ModeloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modelo);
+    }
+}
